55638cbd7cc5722ed7bfe89bfbc58293942e854a
Felipe Gangrel <felipefrog@gmail.com>
2020-09-13 16:10:22 -0300

    update

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,13 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:navigation/models/screen.dart';
 import 'package:navigation/screens/home/home_screen.dart';
 import 'package:navigation/screens/home/profile_screen.dart';
+import 'package:navigation/screens/library/library_screen.dart';
+import 'package:navigation/screens/search/search_screen.dart';
 
 class ScreenIndex {
   static const HOME_SCREEN = 0;
   static const SEARCH_SCREEN = 1;
-  static const LIBRARY_SCREEN = 3;
+  static const LIBRARY_SCREEN = 2;
 }
 
 class NavigationProvider extends ChangeNotifier {
@@ -16,24 +17,45 @@ class NavigationProvider extends ChangeNotifier {
 
   final Map<int, Screen> _screens = {
     ScreenIndex.HOME_SCREEN: Screen(
-      title: 'Home',
-      iconData: Icons.home,
-      child: HomeScreen(),
-      initialRoute: HomeScreen.route,
-      navigatorState: GlobalKey<NavigatorState>(),
-      scrollController: ScrollController(),
-      onGererateRoute: (settings)  {
-        switch(settings.name){
-          case ProfileScreen.route:
-            return MaterialPageRoute(builder: (_) => ProfileScreen());
-          default:
-            return MaterialPageRoute(builder: (_) => HomeScreen());
-        }
-      }
-    ),
+        title: 'Home',
+        iconData: Icons.home,
+        child: HomeScreen(),
+        initialRoute: HomeScreen.route,
+        navigatorState: GlobalKey<NavigatorState>(),
+        scrollController: ScrollController(),
+        onGererateRoute: (settings) {
+          switch (settings.name) {
+            case ProfileScreen.route:
+              return MaterialPageRoute(builder: (_) => ProfileScreen());
+            default:
+              return MaterialPageRoute(builder: (_) => HomeScreen());
+          }
+        }),
     ScreenIndex.SEARCH_SCREEN: Screen(
-      title: 'Search',
-      iconData: Icons.search,
-    )
-  } 
+        title: 'Search',
+        iconData: Icons.search,
+        child: SearchScreen(),
+        initialRoute: SearchScreen.route,
+        navigatorState: GlobalKey<NavigatorState>(),
+        scrollController: ScrollController(),
+        onGererateRoute: (settings) {
+          switch (settings.name) {
+            default:
+              return MaterialPageRoute(builder: (_) => SearchScreen());
+          }
+        }),
+    ScreenIndex.LIBRARY_SCREEN: Screen(
+        title: 'My Library',
+        iconData: Icons.search,
+        child: LibraryScreen(),
+        initialRoute: LibraryScreen.route,
+        navigatorState: GlobalKey<NavigatorState>(),
+        scrollController: ScrollController(),
+        onGererateRoute: (settings) {
+          switch (settings.name) {
+            default:
+              return MaterialPageRoute(builder: (_) => LibraryScreen());
+          }
+        }),
+  };
 }
